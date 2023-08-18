47d0dca34f78afe7bbd1d0a7f53efb07cb55516e
Kevin Quail <kevinq.7365@gmail.com>
2023-08-18 10:48:06 -0500

    AddTeam HTML completed. Added future POST for saving team. Called AddTeam function in main

---
+++ scripts/team/AddTeam.js
@@ -0,0 +1,6 @@
+export const AddTeam = () => {
+  let addTeamHTML = `<div id="addteam_container">`;
+  addTeamHTML += `<input placeholder="Team Name"/>`;
+  addTeamHTML += `<button>Create Team</button>`;
+  return addTeamHTML;
+};
