0f7de372f8cdbdbaf9a445f12e985fea72ddcc36
Tricia Swift <tricia.swift8@gmail.com>
2023-08-18 10:04:08 -0500

    Added Start Button Component Function

---
+++ scripts/score/CurrentGame.js
@@ -0,0 +1,5 @@
+export const CurrentGameBoard = () => {
+  let currentGameHTML = `<div id="currentgame_container">`;
+  currentGameHTML += `<p>No game currently in progress...</p>`;
+  return currentGameHTML;
+};
