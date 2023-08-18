a41e9c27f16b5c78f087c1502913c14255ce7cc2
Aedin Pittman <aedinpittman@gmail.com>
2023-08-18 14:40:28 -0500

    added headers to each leaderboard column

--- scripts/score/LeaderBoard.js
+++ scripts/score/LeaderBoard.js
@@ -5,8 +5,11 @@ export const LeaderBoard = async () => {
   const players = await playerResponse.json();
   const scoreResponse = await fetch("http://localhost:8088/scores");
   const scores = await scoreResponse.json();
-  let htmlString = "<div class='leaderboard-table'>";
+  let htmlString =
+    "<div class='leaderboard-headers'> <h4 class='header'>Team</h4> <h4 class='header'>Players</h4> <h4 class='header'>Scores</h4></div>";
   let count = 0;
+
+  htmlString += "<div class='leaderboard-table'>";
 
   for (const team of teams) {
     htmlString += `<div class="leaderboard-row">
