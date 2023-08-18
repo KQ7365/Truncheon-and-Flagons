21bc80aa877538e6e32eadb31286ecd4d13161fa
Tricia Swift <tricia.swift8@gmail.com>
2023-08-18 13:13:19 -0500

    finished some initial styling on the buttons

--- scripts/team/AddTeam.js
+++ scripts/team/AddTeam.js
@@ -1,6 +1,6 @@
 export const AddTeam = () => {
   let addTeamHTML = `<div id="addteam_container">`;
   addTeamHTML += `<input placeholder="Team Name"/>`;
-  addTeamHTML += `<button>Create Team</button>`;
+  addTeamHTML += `<button class="btn btn--success btn--small">Create Team</button>`;
   return addTeamHTML;
 };
